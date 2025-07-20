7158279a5decd7f3cc4b41119626b6f5b8b6d073
Jatin Kapoor <jatink9599@gmail.com>
2025-07-20 21:04:35 +0530

    Update Navbar.jsx

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,11 +1,10 @@
 import React from 'react';
-import logo from '../assets/logo.svg'; 
 
 const Navbar = () => {
   return (
     <div className="navbar-container">
       <div className="navbar-content">
-        <img src={logo} alt="Logo" className="navbar-logo" />
+        <h2 className="navbar-title">Jatin DevOps Cloud</h2>
       </div>
     </div>
   );
